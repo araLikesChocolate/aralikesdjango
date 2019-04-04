8e159964c85ae0c9be18aca6739971e8c5b2ed3b
ara_likes_chocolate <ara.likes.chocolate@gmail.com>
2019-04-04 09:12:54 +0900

    home 수정

---
+++ newproject/newproject/views.py
@@ -0,0 +1,16 @@
+from django.views.generic.base import TemplateView
+from django.apps import apps
+
+#-- TemplateView
+class HomeView(TemplateView) :
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        #context['app_list'] = ['app1_ara', ]
+        dictVerbose = {}
+        for app in apps.get_app_configs() :
+            if 'site-packages' not in app.path :
+                dictVerbose[app.label] = app.verbose_name
+        context['verbose_dict'] = dictVerbose
+        return context
