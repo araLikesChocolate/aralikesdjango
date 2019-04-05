bc93bab1a86b3416971e660c020023aef677afc1
ara_likes_chocolate <ara.likes.chocolate@gmail.com>
2019-04-05 16:34:30 +0900

    2019-04-05 4:34 setting.py views,py css html 수정

--- newproject/newproject/views.py
+++ newproject/newproject/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.apps import apps
+import json, requests
 
 #-- TemplateView
 class HomeView(TemplateView) :
@@ -13,4 +14,5 @@ class HomeView(TemplateView) :
             if 'site-packages' not in app.path :
                 dictVerbose[app.label] = app.verbose_name
         context['verbose_dict'] = dictVerbose
+        context['marvel_hero'] = json.loads(requests.get("https://simplifiedcoding.net/demos/marvel/").text)
         return context
