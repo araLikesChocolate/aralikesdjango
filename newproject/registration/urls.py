3ac54d55105136bc80ccb87eb2aa24340cbae9e9
jinjuleekr <jinju2415@gmail.com>
2019-04-09 09:44:23 +0900

    네이버 소셜 로그인 기능 추가

--- newproject/registration/urls.py
+++ newproject/registration/urls.py
@@ -7,6 +7,7 @@ app_name = "registration"
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/naver/', views.naver_callback, name='naver_callback'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
