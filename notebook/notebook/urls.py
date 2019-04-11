2dab4115283f7f257c7bbf6cc8c704e5d4c3586e
ara_likes_chocolate <ara.likes.chocolate@gmail.com>
2019-04-11 14:42:23 +0900

    project 이름 변경(notebook)

--- notebook/notebook/urls.py
+++ notebook/notebook/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from newproject import views
+from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
