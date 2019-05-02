4d767e73bb6ea976c0927b42debb4c5157f7d6dc
AraLikesChocolate <44279681+AraLikesChocolate@users.noreply.github.com>
2019-05-02 18:03:59 +0900

    custom_tags 추가

--- notebook/api/templatetags/custom_tags.py
+++ notebook/api/templatetags/custom_tags.py
@@ -1,10 +1,8 @@
 from django import template
-# from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 @register.filter(name='split')
-# @stringfilter
 def split(value, key):
     """
         Returns the value turned into a list.
