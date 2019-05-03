0a4f710f8877276193df98a55b38c3c1834f2302
jinjuleekr <jinju2415@gmail.com>
2019-05-03 11:44:29 +0900

    로딩중 이미지 추가, 필요없는 템플릿 삭제

--- notebook/static/js/upload.js
+++ notebook/static/js/upload.js
@@ -1,7 +1,32 @@
-$(function () {
+function file_get() {
+    console.log("Button Clicked")     
 
-    //***** 로딩 아이콘 수정 예정 */
-    // $('#loading').hide();
+    $.ajax({
+        type: 'GET',
+        url: '/upload',
+        async: false,
+        success: function(data) { //받아오는 것
+            if(data != null) {
+                console.log(data)
+                $("#modalForm").html(data);
+                // $('div').removeClass("modal-backdrop");
+                var m = $('#myModal')
+                m.addClass('show')
+                m.attr('style', 'display:block;')
+                // show  aria-modal="true", style="display:block"
+                $('span.loading').hide();
+            }
+            else {
+                console.log("데이터 받기 실패");
+            }
+        },
+        fail: function(error) {
+            console.log('error');
+        }
+    });  
+}
+
+$(function () {
 
     $("#btnsubmit").click(function(event){
 
@@ -11,8 +36,9 @@ $(function () {
         // data.append("publish", $("input[name=publish]").val());
 
         //***** 로딩 아이콘 수정 예정 */
-        // $('#loading').show();
-
+        $('span.loading').show();
+        $('#img-form').hide();
+        $(".modal-footer").hide();
         var form = $('form')[0];
         //FormData parameter에 담아줌
         var formData = new FormData(form);
@@ -26,6 +52,7 @@ $(function () {
             success:function(data){
                 alert("사진 로드 성공");
                 if(data != null) {
+                    // 업로드 결과 보여주기
                     $("#modalForm").html(data);
                     $(".modal-footer").hide();
                     $(".close").click(function(event){
@@ -42,29 +69,3 @@ $(function () {
     });            
 });  
 
-function test() {
-        console.log("Button Clicked")     
-
-        $.ajax({
-            type: 'GET',
-            url: '/upload',
-            async: false,
-            success: function(data) { //받아오는 것
-                if(data != null) {
-                    console.log(data)
-                    $("#modalForm").html(data);
-                    // $('div').removeClass("modal-backdrop");
-                    var m = $('#myModal')
-                    m.addClass('show')
-                    m.attr('style', 'display:block;')
-                    // show  aria-modal="true", style="display:block"
-                }
-                else {
-                    console.log("데이터 받기 실패");
-                }
-            },
-            fail: function(error) {
-                console.log('error');
-            }
-        });  
-}
